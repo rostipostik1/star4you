5ac6dbef527e09c95f673114315e4c084e191c81
rostipostik1 <roro6178201@gmail.com>
2026-02-16 19:05:10 +0200

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import {Inter, Oswald, Manrope } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const interSans = Inter({
+  variable: "--font-inter",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const geIsOswald = Oswald({
+  variable: "--font-oswald",
+});
+
+const geIsManrope = Manrope({
+  variable: "--font-manrope",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${interSans.variable} ${geIsOswald.variable} ${geIsManrope.variable} antialiased`}
       >
         <main>
           {children}
